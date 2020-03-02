f9eee45b3525bdc548952011a94c78f0d6ed19ef
SaikrishnaTadepalli <58894708+SaikrishnaTadepalli@users.noreply.github.com>
2020-03-01 19:13:19 -0500

    Add files via upload

---
+++ TadepalliS_MethodsEx02Craps/TadepalliS_MethodsEx02Craps/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace TadepalliS_MethodsEx02Craps
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCraps = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.lstOut = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnCraps
+            // 
+            this.btnCraps.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCraps.Location = new System.Drawing.Point(12, 12);
+            this.btnCraps.Name = "btnCraps";
+            this.btnCraps.Size = new System.Drawing.Size(600, 60);
+            this.btnCraps.TabIndex = 0;
+            this.btnCraps.Text = "Craps";
+            this.btnCraps.UseVisualStyleBackColor = true;
+            this.btnCraps.Click += new System.EventHandler(this.btnCraps_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(12, 363);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(600, 60);
+            this.btnExit.TabIndex = 1;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // lstOut
+            // 
+            this.lstOut.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstOut.FormattingEnabled = true;
+            this.lstOut.ItemHeight = 25;
+            this.lstOut.Location = new System.Drawing.Point(12, 78);
+            this.lstOut.Name = "lstOut";
+            this.lstOut.Size = new System.Drawing.Size(600, 279);
+            this.lstOut.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightCoral;
+            this.ClientSize = new System.Drawing.Size(624, 437);
+            this.Controls.Add(this.lstOut);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnCraps);
+            this.Name = "Form1";
+            this.Text = "Game Of Craps";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCraps;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.ListBox lstOut;
+    }
+}
+
---
+++ TadepalliS_MethodsEx02Craps/TadepalliS_MethodsEx02Craps/Form1.cs
@@ -0,0 +1,92 @@
+﻿/************************************************************
+    PROGRAMME	:	Methods Ex02 Craps
+
+    OUTLINE		:	THis program rolls 2 dice, and if the sum 
+                    of the dice is 7 or 11 on the first throw
+                    the player wins. If the sum is 2, 3 or 12 
+                    on the first throw then the player looses.
+                    If the sum is 4, 5, 6, 7, 8, 9 or 10 on the 
+                    first throw, the player keeps rolling until
+                    the same sum is achieved. 
+
+    PROGRAMMER	:	Saikrishna Tadepalli
+
+    DATE		:	October 24, 2019
+ ************************************************************/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TadepalliS_MethodsEx02Craps
+{
+    public partial class Form1 : Form
+    {
+        Random rand = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCraps_Click(object sender, EventArgs e)
+        {
+            lstOut.Items.Clear();
+            int point = 0;
+            int rollSum = 0;
+
+            roll(ref rollSum);
+
+            if (rollSum == 7 || rollSum == 11)
+                print("Win");
+            else if (rollSum == 2 || rollSum == 3 || rollSum == 12)
+                print("Lose");
+            else
+            {
+                point = rollSum;
+                lstOut.Items.Add("");
+                lstOut.Items.Add("Point = " + point);
+                lstOut.Items.Add("");
+                do
+                {
+                    roll(ref rollSum);
+
+                    if (rollSum == 7)
+                        break;
+
+                } while ((point != rollSum));
+
+                if (rollSum == 7)
+                    print("Lose");
+                else
+                    print("Win");
+
+            }
+        }
+        private void print(string message)
+        {
+            lstOut.Items.Add("");
+            lstOut.Items.Add("You " + message + "!");
+        }
+        private void roll(ref int sum)
+        {
+            int[] rolls = { rand.Next(1, 7), rand.Next(1, 7)};
+            sum = rolls[0] + rolls[1];
+            lstOut.Items.Add("Player Rolled " + rolls[0]+ " and " + rolls[1] + " = " + sum);
+        }
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
