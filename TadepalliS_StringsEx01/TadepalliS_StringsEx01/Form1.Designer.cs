909ce6cdab581f3b29200c4c17e1921047884fb7
SaikrishnaTadepalli <58894708+SaikrishnaTadepalli@users.noreply.github.com>
2020-03-01 19:14:00 -0500

    Add files via upload

---
+++ TadepalliS_StringsEx01/TadepalliS_StringsEx01/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace TadepalliS_StringsEx01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.txtOut = new System.Windows.Forms.TextBox();
+            this.btnFindIt = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.BackColor = System.Drawing.Color.Silver;
+            this.lblTitle.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblTitle.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.Location = new System.Drawing.Point(12, 9);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(300, 50);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "Enter a String:";
+            this.lblTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtInput
+            // 
+            this.txtInput.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtInput.Location = new System.Drawing.Point(12, 69);
+            this.txtInput.Multiline = true;
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(300, 200);
+            this.txtInput.TabIndex = 1;
+            // 
+            // txtOut
+            // 
+            this.txtOut.BackColor = System.Drawing.Color.CadetBlue;
+            this.txtOut.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtOut.Location = new System.Drawing.Point(12, 331);
+            this.txtOut.Multiline = true;
+            this.txtOut.Name = "txtOut";
+            this.txtOut.ReadOnly = true;
+            this.txtOut.Size = new System.Drawing.Size(300, 100);
+            this.txtOut.TabIndex = 2;
+            // 
+            // btnFindIt
+            // 
+            this.btnFindIt.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFindIt.Location = new System.Drawing.Point(12, 275);
+            this.btnFindIt.Name = "btnFindIt";
+            this.btnFindIt.Size = new System.Drawing.Size(300, 50);
+            this.btnFindIt.TabIndex = 3;
+            this.btnFindIt.Text = "Find It";
+            this.btnFindIt.UseVisualStyleBackColor = true;
+            this.btnFindIt.Click += new System.EventHandler(this.btnFindIt_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnFindIt;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightCoral;
+            this.ClientSize = new System.Drawing.Size(326, 441);
+            this.Controls.Add(this.btnFindIt);
+            this.Controls.Add(this.txtOut);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.lblTitle);
+            this.Name = "Form1";
+            this.Text = "Most Frequent Character";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.TextBox txtOut;
+        private System.Windows.Forms.Button btnFindIt;
+    }
+}
+
---
+++ TadepalliS_StringsEx01/TadepalliS_StringsEx01/Form1.cs
@@ -0,0 +1,55 @@
+﻿/************************************************************
+    PROGRAMME	:	Strings Ex01 Most Frequent Character
+
+    OUTLINE		:	This programme asks the user for a string 
+                    input and outputs the most frequently 
+                    occurring letter in the text inputed.
+
+
+    PROGRAMMER	:	Saikrishna Tadepalli
+
+    DATE		:	November 10th 2019
+************************************************************/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TadepalliS_StringsEx01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.KeyPreview = true;
+        }
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+                Application.Exit();
+        }
+        private void btnFindIt_Click(object sender, EventArgs e)
+        {
+            string input = (txtInput.Text).Replace(" ", String.Empty);
+            int[] repeats = new int[input.Length];
+
+            for (int i = 0; i < input.Length; i++)
+                for (int j = 0; j < input.Length; j++)
+                    if (input[i] == input[j])
+                        repeats[i] += 1;
+
+            txtOut.Text = "The most frequent character is \'" + input[Array.IndexOf(repeats,(repeats.Max()))] + "\'.";
+        }
+    }
+}
