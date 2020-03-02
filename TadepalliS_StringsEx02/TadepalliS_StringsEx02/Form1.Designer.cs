3006c7f19d6cf62616ab49ede0e8b48f84388d3e
SaikrishnaTadepalli <58894708+SaikrishnaTadepalli@users.noreply.github.com>
2020-03-01 19:14:55 -0500

    Add files via upload

---
+++ TadepalliS_StringsEx02/TadepalliS_StringsEx02/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace TadepalliS_StringsEx02
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstOut = new System.Windows.Forms.ListBox();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.btnProcess = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lstOut
+            // 
+            this.lstOut.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstOut.FormattingEnabled = true;
+            this.lstOut.ItemHeight = 25;
+            this.lstOut.Location = new System.Drawing.Point(12, 44);
+            this.lstOut.Name = "lstOut";
+            this.lstOut.Size = new System.Drawing.Size(200, 679);
+            this.lstOut.TabIndex = 0;
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblTitle.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.Location = new System.Drawing.Point(218, 44);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(800, 62);
+            this.lblTitle.TabIndex = 1;
+            this.lblTitle.Text = "Letter Count";
+            this.lblTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtInput
+            // 
+            this.txtInput.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtInput.Location = new System.Drawing.Point(218, 109);
+            this.txtInput.Multiline = true;
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(800, 455);
+            this.txtInput.TabIndex = 2;
+            // 
+            // btnProcess
+            // 
+            this.btnProcess.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnProcess.Location = new System.Drawing.Point(218, 570);
+            this.btnProcess.Name = "btnProcess";
+            this.btnProcess.Size = new System.Drawing.Size(200, 50);
+            this.btnProcess.TabIndex = 3;
+            this.btnProcess.Text = "Process";
+            this.btnProcess.UseVisualStyleBackColor = true;
+            this.btnProcess.Click += new System.EventHandler(this.btnProcess_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnExit.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(818, 570);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(200, 50);
+            this.btnExit.TabIndex = 4;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnProcess;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightCoral;
+            this.CancelButton = this.btnExit;
+            this.ClientSize = new System.Drawing.Size(1028, 776);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnProcess);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.lstOut);
+            this.Name = "Form1";
+            this.Text = "Text Processing";
+            this.Load += new System.EventHandler(this.k);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstOut;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Button btnProcess;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ TadepalliS_StringsEx02/TadepalliS_StringsEx02/Form1.cs
@@ -0,0 +1,70 @@
+﻿/************************************************************
+    PROGRAMME	:	Strings Ex02 Text Processing
+
+    OUTLINE		:	This programme reads the text from the file 
+                    and outputs the number of lines of text and 
+                    the number of times each letter appears in 
+                    the text.
+
+    PROGRAMMER	:	Saikrishna Tadepalli
+
+    DATE		:	November 10th 2019
+************************************************************/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+namespace TadepalliS_StringsEx02
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.KeyPreview = true;
+            txtInput.ReadOnly = true;
+        }
+        private void k(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+                Application.Exit();
+        }
+
+        private void btnProcess_Click(object sender, EventArgs e)
+        {
+            lstOut.Items.Clear();
+            string[] lines = File.ReadAllLines("txtIn.txt");
+            txtInput.Text = File.ReadAllText("txtIn.txt");
+            string[] letters = ("A B C D E F G H I J K L M N O P Q R S T U V W X Y Z").Split(Convert.ToChar(" "));
+            int[] letterCount = new int[letters.Length];
+
+            for (int i = 0; i < letters.Length; i++)
+            {
+                for (int j = 0; j < lines.Length; j++)
+                    for (int k = 0; k < lines[j].Length; k++)
+                        if (letters[i] == lines[j][k].ToString().ToUpper())
+                            letterCount[i] += 1;
+                lstOut.Items.Add(letters[i] + " count = " + letterCount[i]);
+            }
+
+            lstOut.Items.Add("No. of lines = " + lines.Length);
+
+        }
+    }
+}
