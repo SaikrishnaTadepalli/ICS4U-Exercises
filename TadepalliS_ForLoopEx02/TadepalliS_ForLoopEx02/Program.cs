8a32be1bac2f34f1b90d5df6f196863510d50a3e
SaikrishnaTadepalli <58894708+SaikrishnaTadepalli@users.noreply.github.com>
2020-03-01 19:07:35 -0500

    Add files via upload

---
+++ TadepalliS_ForLoopEx02/TadepalliS_ForLoopEx02/Program.cs
@@ -0,0 +1,40 @@
+﻿/************************************************************
+   PROGRAMME	:	For Loop Ex02 Odd Numbers
+
+   OUTLINE		:	This programme asks the user to input a number 
+                    from which the program output all odd numbers 
+                    from 0 to the entered number
+
+   PROGRAMMER	:	Saikrishna Tadepalli
+
+   DATE		:	September 24, 2019
+************************************************************/
+
+using System;
+
+namespace TadepalliS_ForLoopEx02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "For Loop Ex02: Odd Numbers";
+            Console.ForegroundColor = ConsoleColor.Gray;
+            
+            int input;
+
+            Console.Write("\n\tEnter a Positive Number: ");
+
+            input = int.Parse(Console.ReadLine());
+
+            Console.Write("\n\n");
+
+            for (int i = 1; i <= input; i += 2)
+            {
+                Console.Write("\t"+ i);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
